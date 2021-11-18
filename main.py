5361427d1f9322910eb25f361fcf8511fbb343ed
Mohnish <78406326+M-jpg-ai@users.noreply.github.com>
2021-11-18 14:17:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# Dont delete the next line
+
+
+print("Subscribe Codex")
+print("Like this video")
+"""
+this 
+is
+a 
+Multi line
+comment
+"""
+print("Mohnish is a good boy.\n He Loves Coding\tHe is An expert In javascript")
